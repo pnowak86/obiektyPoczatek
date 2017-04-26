b025c81dfacb7680a8780f4b2de8a86507066456
pnowak86 <p_nowak86@poczta.fm>
2017-04-26 19:50:17 +0200

    Obiekty 19:50 26.04.2017

--- src/main/java/org/mytools/myclasses/App.java
+++ src/main/java/org/mytools/myclasses/App.java
@@ -11,7 +11,9 @@ public class App
 
         Car myCar = new Car(3,"mercedes");
         Car myCar2 = new Car("opel");
-        myCar2.setWheelCount(5);
+        int wheels = 7;
+
+        myCar2.setWheelCount(wheels);
 
         System.out.println(myCar.toString());
         System.out.println(myCar2.toString());
@@ -23,6 +25,19 @@ public class App
         System.out.println("myCar2: " + myCar2.getWheelCount()+ " " +  "wheels");
         System.out.println("myCar2 is " + myCar2.getBrand());
 
+        System.out.println(myCar == myCar2);
+        Car another = myCar2;//kopia referencji
+        System.out.println("==========");
+        System.out.println(myCar2.toString());
+        System.out.println(another.toString());
+
+        myCar2.setBrand("bmw");
+        System.out.println(another.getBrand());
+        System.out.println(myCar2 == another);
+
+
+        myCar = myCar2;//nadpisujemy referencje od tego momentu myCar pokazuje na myCar2
+        System.out.println(myCar);
 
 
         //myCar.similarCar.doBlink();
